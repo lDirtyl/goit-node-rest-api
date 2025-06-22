89c58a68e0304e0ac3b72365fa7a7dfd4cf80e01
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2025-06-22 09:37:29 +0200

    update contactsServices

---
+++ services/contactsServices.js
@@ -0,0 +1,40 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("db", "contacts.json");
+const updateContact = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+export const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+};
+
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const res = contacts.find((item) => item.id === contactId);
+  return res || null;
+};
+
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) return null;
+  const [result] = contacts.splice(idx, 1);
+  await updateContact(contacts);
+  return result;
+};
+
+export const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContact(contacts);
+  return newContact;
+};
