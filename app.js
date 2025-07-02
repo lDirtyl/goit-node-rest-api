e7d00d19241adc232354737263bf4f493ef70265
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2025-07-02 08:52:59 +0200

    finish

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from "node:path";
 import "dotenv/config";
 import dotenv from "dotenv";
 
@@ -13,6 +14,7 @@ const app = express();
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve("public")));
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/auth", authRouter);
